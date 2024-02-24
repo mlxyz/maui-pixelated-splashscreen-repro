cb87222abeee10ce5047d634500688b1b0e07cfb
Marian Lambert <marian.lambert@xpace.de>
2024-02-24 14:50:26 +0100

    Add project files.

---
+++ PixelatedSplashScreenRepro/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PixelatedSplashScreenRepro
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PixelatedSplashScreenRepro/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PixelatedSplashScreenRepro
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
